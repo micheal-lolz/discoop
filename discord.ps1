0c713bf19283d3aacbca0d3777577514a9b771a0
koredev <femcheal6969@gmail.com>
2024-09-16 10:13:48 +0200

    deleting useless debug comments

--- discord.ps1
+++ discord.ps1
@@ -1,18 +1,13 @@
-# Define the URL for the Discord setup file
 $discordUrl = "https://discord.com/api/download?platform=win"
 
-# Define the path to download the installer
 $installerPath = "$env:TEMP\DiscordSetup.exe"
 
-# Download the Discord installer
 Write-Host "Downloading Discord..."
 Invoke-WebRequest -Uri $discordUrl -OutFile $installerPath
 
-# Install Discord
 Write-Host "Installing Discord..."
 Start-Process -FilePath $installerPath -ArgumentList "/verysilent" -Wait
 
-# Clean up
 Write-Host "Cleaning up..."
 Remove-Item -Path $installerPath
 
